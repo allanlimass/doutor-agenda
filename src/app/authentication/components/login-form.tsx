8415474381b9ca9fb258c8740860e67e48d68d73
Allan Silva <allanlimass@hotmail.com>
2026-02-21 10:16:07 -0300

    feat: add login

--- src/app/authentication/components/login-form.tsx
+++ src/app/authentication/components/login-form.tsx
@@ -1,7 +1,10 @@
 "use client";
 
 import { zodResolver } from "@hookform/resolvers/zod";
+import { Loader2 } from "lucide-react";
+import { useRouter } from "next/navigation";
 import { Controller, useForm } from "react-hook-form";
+import { toast } from "sonner";
 import z from "zod";
 
 import { Button } from "@/components/ui/button";
@@ -20,6 +23,7 @@ import {
   FieldLabel,
 } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
+import { authClient } from "@/lib/auth-client";
 
 const loginSchema = z.object({
   email: z
@@ -34,6 +38,7 @@ const loginSchema = z.object({
 });
 
 export default function LoginForm() {
+  const router = useRouter();
   const form = useForm<z.infer<typeof loginSchema>>({
     resolver: zodResolver(loginSchema),
     defaultValues: {
@@ -42,8 +47,21 @@ export default function LoginForm() {
     },
   });
 
-  function onSubmit(values: z.infer<typeof loginSchema>) {
-    console.log(values);
+  async function onSubmit(values: z.infer<typeof loginSchema>) {
+    await authClient.signIn.email(
+      {
+        email: values.email,
+        password: values.password,
+      },
+      {
+        onSuccess: () => {
+          router.push("/dashboard");
+        },
+        onError: () => {
+          toast.error("E-mail ou senha inválidos.");
+        },
+      },
+    );
   }
 
   return (
@@ -100,7 +118,11 @@ export default function LoginForm() {
         <CardFooter>
           <Field orientation="horizontal">
             <Button className="w-full" type="submit">
-              Entrar
+              {form.formState.isSubmitting ? (
+                <Loader2 className="mr-2 h-4 animate-spin" />
+              ) : (
+                "Entrar"
+              )}
             </Button>
           </Field>
         </CardFooter>
