7b69a920a12bba3666393f33a9c33dba78fb1493
Allan Silva <allanlimass@hotmail.com>
2026-02-24 16:42:13 -0300

    feat: create page container components

--- src/app/(private)/_components/app-sidebar.tsx
+++ src/app/(private)/_components/app-sidebar.tsx
@@ -9,7 +9,7 @@ import {
 } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import {
@@ -58,6 +58,9 @@ const items = [
 export function AppSidebar() {
   const router = useRouter();
   const session = authClient.useSession();
+  const pathname = usePathname();
+
+  console.log(pathname);
 
   const handleSignOut = async () => {
     await authClient.signOut({
@@ -80,7 +83,7 @@ export function AppSidebar() {
             <SidebarMenu>
               {items.map((item) => (
                 <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild>
+                  <SidebarMenuButton asChild isActive={pathname === item.url}>
                     <Link href={item.url}>
                       <item.icon />
                       <span>{item.title}</span>
